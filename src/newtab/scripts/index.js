fa44d291759fe0980756df3830f7eee36450b009
Kim HyunWoo <stories2@naver.com>
2023-10-07 18:40:16 +0900

    Tab link logic added

--- src/newtab/scripts/index.js
+++ src/newtab/scripts/index.js
@@ -28,8 +28,8 @@ function getAllTabsFromAllWindows() {
   });
 }
 
-async function simpleQuery(inputTxt) {
-  const tabs = await getAllTabsFromAllWindows();
+function simpleQuery(inputTxt) {
+  const tabs = selectAllTabsFromDB();
   const filteredTabs = tabs.filter(
     (tab) =>
       (tab.title || "").includes(inputTxt) || (tab.url || "").includes(inputTxt)
@@ -37,10 +37,10 @@ async function simpleQuery(inputTxt) {
   appendTabsToTabList(filteredTabs);
 }
 
-async function onSearchInputChanges(e) {
+function onSearchInputChanges(e) {
   console.log(e.target.value);
   resetTabsFromTabList();
-  await simpleQuery(e.target.value);
+  simpleQuery(e.target.value);
 }
 
 function getTabListLayoutsByScreenSize() {
@@ -95,7 +95,38 @@ function resetTabsFromTabList() {
   tabListLayouts.xs.forEach((tabList) => tabList.replaceChildren());
 }
 
+const tabDB = {};
+
 window.onload = async function () {
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    console.log("changeInfo status", changeInfo.status, changeInfo, tab);
+    if (changeInfo.status === "loading") {
+      const { openerTabId, title, url, id } = tab;
+      if (typeof openerTabId !== "undefined") {
+        console.log(
+          "open in new tab. parent:",
+          openerTabId,
+          " id:",
+          id,
+          title,
+          url
+        );
+        linkChildTabAndParentTab(openerTabId, id);
+        insertTabsToDB([tab]);
+        console.log("parent:", tabDB[openerTabId]);
+        console.log("child:", tabDB[id]);
+      } else {
+        updateTabsToDB([tab]);
+        console.log("opened|restored tab url updated. id:", id, title, url);
+        console.log("tab db:", tabDB[id]);
+      }
+    }
+  });
+  chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+    // console.log(tabId, removeInfo);
+    updateTabStatus(tabId, "removed");
+    console.log("rm tab:", tabDB[tabId]);
+  });
   document
     .getElementById("inputSearchBar")
     .addEventListener("change", onSearchInputChanges);
@@ -104,6 +135,40 @@ window.onload = async function () {
     e.preventDefault();
   });
   const tabs = await getAllTabsFromAllWindows();
-  console.log("tabs", tabs);
-  appendTabsToTabList(tabs);
+  //   console.log("tabs", tabs);
+  insertTabsToDB(tabs);
+  console.log(tabDB);
+  appendTabsToTabList(selectAllTabsFromDB());
 };
+
+function insertTabsToDB(tabs = []) {
+  tabs.forEach((tab) => ((tab["children"] = []), (tabDB[tab.id] = tab)));
+}
+
+function updateTabsToDB(tabs = []) {
+  tabs.forEach((tab) => {
+    tabDB[tab.id] = {
+      children: [],
+      ...tab,
+      ...(tabDB[tab.id] || {}),
+    };
+  });
+}
+
+/**
+ * @param {number} tabId
+ * @param {string} status complete|loading|removed|unloaded
+ */
+function updateTabStatus(tabId, status = "complete") {
+  tabDB[tabId].status = status;
+}
+
+function linkChildTabAndParentTab(parentTabId, childTabId) {
+  if (!tabDB[parentTabId]["children"].includes(childTabId))
+    tabDB[parentTabId]["children"].push(childTabId);
+  else console.warn(parentTabId, "<->", childTabId, " already linked");
+}
+
+function selectAllTabsFromDB() {
+  return Object.keys(tabDB).map((tabId) => tabDB[tabId]);
+}
