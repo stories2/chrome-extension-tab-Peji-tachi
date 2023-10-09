9d2022eac50ed106c0e3f1ea93dc51f3a4198529
Kim HyunWoo <stories2@naver.com>
2023-10-09 14:15:23 +0900

    Add bootstrap

--- peji-tachi-tab/src/main.ts
+++ peji-tachi-tab/src/main.ts
@@ -1,11 +1,15 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+
+import 'bootstrap/dist/js/bootstrap'
