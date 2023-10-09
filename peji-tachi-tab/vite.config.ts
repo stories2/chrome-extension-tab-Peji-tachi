8ed0a1a94239cb92cd4794dddc7c4086385f4f33
Kim HyunWoo <stories2@naver.com>
2023-10-09 13:26:25 +0900

    Update vite.config.ts

--- peji-tachi-tab/vite.config.ts
+++ peji-tachi-tab/vite.config.ts
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: process.env.NODE_ENV === 'production' ? '/newtab' : '/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
